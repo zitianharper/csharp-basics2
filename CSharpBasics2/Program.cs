9b620cb05cbc3149f6ba4879007d3b4097c4b766
Zitian Harper <Zitian.harper@icloud.com>
2026-03-11 15:44:29 +0000

    Initial commit

---
+++ CSharpBasics2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpBasics2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
